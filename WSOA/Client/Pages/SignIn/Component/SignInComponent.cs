b1bcfc727e6c0a9fb0e4f86990e5e9f703fc96d8
Potrunks <potrunks@hotmail.com>
2023-08-06 22:48:30 +0200

    Init Sing In Page #4

---
+++ WSOA/Client/Pages/SignIn/Component/SignInComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using WSOA.Shared.ViewModel;
+
+namespace WSOA.Client.Pages.SignIn.Component
+{
+    public class SignInComponent : ComponentBase
+    {
+        public SignInFormViewModel _signInFormVM = new SignInFormViewModel();
+
+        public void SignIn()
+        {
+
+        }
+    }
+}
---
+++ WSOA/Shared/ViewModel/SignInFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WSOA.Shared.ViewModel
+{
+    public class SignInFormViewModel
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
