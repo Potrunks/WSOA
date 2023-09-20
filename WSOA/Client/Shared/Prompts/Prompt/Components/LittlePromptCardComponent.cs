6ab6dd5d2f5fc7e973ab2ac4cc83782bdbcf6f62
Potrunks <potrunks@hotmail.com>
2023-09-20 23:22:27 +0200

    Mise en place de la page d'erreur principale #18

--- WSOA/Client/Shared/Prompts/Prompt/Components/LittlePromptCardComponent.cs
+++ WSOA/Client/Shared/Prompts/Prompt/Components/LittlePromptCardComponent.cs
@@ -5,9 +5,14 @@ namespace WSOA.Client.Shared.Prompts.Prompt.Components
     public class LittlePromptCardComponent : ComponentBase
     {
         [Parameter]
+        [EditorRequired]
         public string LogoPath { get; set; }
 
         [Parameter]
+        [EditorRequired]
         public string Message { get; set; }
+
+        [Parameter]
+        public bool IsErrorMessage { get; set; }
     }
 }
