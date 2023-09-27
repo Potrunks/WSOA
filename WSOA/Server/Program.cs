e1552edf46acb110c8c28e20f27cec2b3792be58
Potrunks <potrunks@hotmail.com>
2023-09-28 01:10:03 +0200

    Ajout console writeline dans program #23

--- WSOA/Server/Program.cs
+++ WSOA/Server/Program.cs
@@ -5,16 +5,22 @@ using WSOA.Server.Data;
 using WSOA.Server.Data.Implementation;
 using WSOA.Server.Data.Interface;
 
+Console.WriteLine("Starting WSOA Server...");
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string dbConnectionString = builder.Configuration.GetConnectionString("WSOA_DB_DEV");
+Console.WriteLine("Preparing connection to database...");
+string dbConnectionString = builder.Configuration.GetConnectionString("WSOA_DB");
 
 builder.Services.AddDbContext<WSOADbContext>(options =>
 {
     options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString));
 });
+string currentDatabase = dbConnectionString.Split(";")[1].Split("=")[1];
+Console.WriteLine("Database connection OK to " + currentDatabase);
 
+Console.WriteLine("Creating all singletons services and repositories...");
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IMenuRepository, MenuRepository>();
@@ -28,16 +34,21 @@ builder.Services.AddScoped<IMailService, MailService>();
 builder.Services.AddScoped<IAccountBusiness, AccountBusiness>();
 builder.Services.AddScoped<IMenuBusiness, MenuBusiness>();
 builder.Services.AddScoped<ITournamentBusiness, TournamentBusiness>();
+Console.WriteLine("Singletons creation OK");
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 builder.Services.AddDistributedMemoryCache();
+
+Console.WriteLine("Creating session...");
 builder.Services.AddSession(option =>
 {
     option.IdleTimeout = TimeSpan.FromMinutes(30);
 });
+Console.WriteLine("Session creation OK with timeout of " + 30 + " minutes");
 
+Console.WriteLine("Building app...");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -64,5 +75,7 @@ app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.UseSession();
+Console.WriteLine("Build app OK");
 
+Console.WriteLine("WSOA Server is running");
 app.Run();
