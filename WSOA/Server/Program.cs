58652c0c4902afbfc9aacb3ab9193f6800f5fa24
Potrunks <potrunks@hotmail.com>
2023-09-29 00:28:38 +0200

    Test migration #23

--- WSOA/Server/Program.cs
+++ WSOA/Server/Program.cs
@@ -5,22 +5,17 @@ using WSOA.Server.Data;
 using WSOA.Server.Data.Implementation;
 using WSOA.Server.Data.Interface;
 
-Console.WriteLine("Starting WSOA Server...");
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-Console.WriteLine("Preparing connection to database...");
+// Database configuration
 string dbConnectionString = builder.Configuration.GetConnectionString("WSOA_DB");
-
 builder.Services.AddDbContext<WSOADbContext>(options =>
 {
     options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString));
 });
-string currentDatabase = dbConnectionString.Split(";")[1].Split("=")[1];
-Console.WriteLine("Database connection OK to " + currentDatabase);
+//
 
-Console.WriteLine("Creating all singletons services and repositories...");
+// Singleton configuration
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IMenuRepository, MenuRepository>();
@@ -34,21 +29,18 @@ builder.Services.AddScoped<IMailService, MailService>();
 builder.Services.AddScoped<IAccountBusiness, AccountBusiness>();
 builder.Services.AddScoped<IMenuBusiness, MenuBusiness>();
 builder.Services.AddScoped<ITournamentBusiness, TournamentBusiness>();
-Console.WriteLine("Singletons creation OK");
+//
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 builder.Services.AddDistributedMemoryCache();
 
-Console.WriteLine("Creating session...");
 builder.Services.AddSession(option =>
 {
     option.IdleTimeout = TimeSpan.FromMinutes(30);
 });
-Console.WriteLine("Session creation OK with timeout of " + 30 + " minutes");
 
-Console.WriteLine("Building app...");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -75,7 +67,5 @@ app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.UseSession();
-Console.WriteLine("Build app OK");
 
-Console.WriteLine("WSOA Server is running");
 app.Run();
